098189a7f0b67e4f1a42d230e2828322d0dd69fb
zainab bibi <ismailzainab719@gmail.com>
2024-04-03 14:04:12 +0500

    Adding a cli-number-guessing-game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answrer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "enter your guess number(Number limit from 1 to 5)",
+    },
+]);
+if (answrer.userGuessNumber === randomNumber) {
+    console.log("Congratulations ! You guess a right number.");
+}
+else {
+    console.log("sorry, you guess a wrong number");
+}
